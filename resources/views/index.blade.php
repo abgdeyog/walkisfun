c465b2627309012feb416e7481638d1d2d3c84e6
Nastya <anastasia.vorton@gmail.com>
2017-10-20 20:00:05 +0300

    Index page creation

---
+++ resources/views/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<header class="Header" role="banner">
+    <div class="Header-Content">
+        <h1 class="Header-Logo" title="Walk is fun" style="padding-top: 5%">WalkIsFun</h1>
+    </div>
+</header>
+
+<div class="container-fluid text-left">
+    <div class="container content">
+        <div class="row">
+            <div class=" col-sm-4">
+            </div>
+            <div class=" col-sm-4 text-center ">
+                <h2 class="slogan text-center">Строим самые классные маршруты</h2>
+                <p>Добро пожаловать на лучший сервис построение интересных именно Вам маршрутов!
+                    Всего один клик отделяет Вас от незабывыаемого путешествия по новому городу.
+                    А может быть вы хотите получше узнать место, в котором живете уже очень давно, но все никак не
+                    находили времени чтобы выбраться на прогулку?
+                    Хватит ждать, жмите на кнопку и скорее в путь!</p>
+                <button class="vkAutorise" id="vkAutorise"><i class="fa fa-vk" aria-hidden="true"></i>
+                    <h3>Войти через VK</h3>
+                </button>
+            </div>
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+</div>
+</div>
+
+<footer class="text-center">
+    <p>Walk Is Fun, 2017</p>
+</footer>
+
+<!--JS section-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+        crossorigin="anonymous"></script>
+</body>
+        </html>
