bf080fa219280b2e7ffa385a28401cba07b02327
Nastya <anastasia.vorton@gmail.com>
2017-10-20 23:48:51 +0300

    Home page with a start form added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<header class="Header" role="banner">
+    <div class="Header-Content" style="padding-top: 25px;">
+        <h1 class="Header-Logo" title="Walk is fun">WalkIsFun</h1>
+    </div>
+</header>
+
+<div class="container-fluid text-left">
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4 col-sm-12 text-center form ">
+                <div class="text-center" style="padding:50px 0">
+                    <div class="logo">Начинаем стоить маршрут</div>
+                    <!-- Main Form -->
+                    <div class="positions-form">
+                        <form id="pos-form" class="text-left">
+                            <div class="main-pos-form">
+                                <div class="positions-group">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="start_place" name="start_place"
+                                               placeholder="Начальная позиция">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="end_place" name="end_place"
+                                               placeholder="Конечная позиция">
+                                    </div>
+                                </div>
+                                <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- end:Main Form -->
+                </div>
+            </div>
+            <div class="col-md-4">
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="text-center">
+    <p>Walk Is Fun, 2017</p>
+</footer>
+</div>
+
+
+<!--JS section-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+        crossorigin="anonymous"></script>
+</body>
+        </html>
