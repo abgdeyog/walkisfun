fd2e78e279da1bc3545081790426247684846499
Artur Boykov <hello@tarex.me>
2017-10-22 01:59:59 +0300

    Like/dislike fixing, new weight calculation.

--- app/Route/PlaceWeight.php
+++ app/Route/PlaceWeight.php
@@ -6,16 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlaceWeight extends Model
 {
-    public $fillable = ['placeID', 'publicID', 'weight'];
-
-    public $placeID;
-    public $publicID;
-    public $weight;
-
-    public function __construct($placeID, $publicID, $weight = 0)
-    {
-        $this->publicID = $publicID;
-        $this->placeID = $placeID;
-        $this->weight = $weight;
-    }
+    public $fillable = ['place_id', 'public_id', 'weight'];
 }
