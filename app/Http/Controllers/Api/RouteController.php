9609e26df974c853608ee2db614373f6d7393e54
Artur Boykov <hello@tarex.me>
2017-10-22 02:00:13 +0300

    Route building

--- app/Http/Controllers/Api/RouteController.php
+++ app/Http/Controllers/Api/RouteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Route\RouteProcessor;
 use Illuminate\Http\Request;
 
 class RouteController extends CoreApiMethodController
@@ -12,7 +13,9 @@ class RouteController extends CoreApiMethodController
 
         $requiredFields = $this->bindRequiredFields($requiredParams, $request);
 
-        return $this->successReturn(['ok']);  // TODO Implement this
+        $route = new RouteProcessor($requiredFields['from'], $requiredFields['to'], $requiredFields['categories'], $requiredFields['time']);
+
+        return $this->successReturn($route->buildRoute());
     }
 
     public function estimateTime(Request $request)
