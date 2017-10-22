a76741f043041baddba95ccf677f45d58bde6508
Artur Boykov <hello@tarex.me>
2017-10-22 09:29:36 +0300

    Some fixes

--- resources/views/finish.blade.php
+++ resources/views/finish.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="stylesheet" href="/css/bootstrap.css"/>
     <link rel="stylesheet" href="/css/main.css"/>
-    <link rel="stylesheet" href="/css/easy-autocomplete.min.css"/>
-    <link rel="stylesheet" href="/css/easy-autocomplete.themes.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet"/>
@@ -15,7 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
-<body>
+<body class="finish-page">
 
 <header class="Header" role="banner">
     <div class="Header-Content" style="padding-top: 25px;">
@@ -28,45 +26,21 @@
         <div class="row">
             <div class="col-md-3">
             </div>
-            <div class="col-md-6 col-sm-12 text-center form ">
-                <div class="text-center" style="padding:50px 0">
-                    <div class="logo">Оцените предложеный вам маршрут! Просто пойставьте "лайк" если место вам
-                        понравилось
-                        или "дизлайк" если оно не совем вписалось в этот маршрут. Нам важно знать ваше мнение,
-                        так мы научим наш сервис подбирать самые классные маршруты!
+            <div class="col-md-6 col-sm-12 text-center form">
+                <div class="main-block">
+                    <div class="logo">
+                        <p><b>Поздравляем, ты справился и закончил маршрут!</b></p>
+                        <p>Понравились ли тебе предложенные места?</p>
+                        <p>Нам важно знать твое мнение, и с каждым голосом сервис будет предлагать все более классные
+                            маршруты!</p>
                     </div>
                     <!-- Main Form -->
                     <div class="places-grade-wrapper">
-                        <div class="gradePlace">
-                            <div class="gradeDescription">
-                                <h4 class="text-left">Название</h4>
-                            </div>
-                            <div class="gradeButtons">
-                                <a class="thumbs-up" href="" title="Да">Да</a>
-                                <a class="thumbs-down" href="" title="Нет">Нет</a>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="gradePlace">
-                            <div class="gradeDescription">
-                                <h4 class="text-left">Название</h4>
-                            </div>
-                            <div class="gradeButtons">
-                                <a class="thumbs-up" href="" title="Да">Да</a>
-                                <a class="thumbs-down" href="" title="Нет"> Нет</a>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="gradePlace">
-                            <div class="gradeDescription">
-                                <h4 class="text-left">Название</h4>
-                            </div>
-                            <div class="gradeButtons">
-                                <a class="thumbs-up" href="" title="Да">Да</a>
-                                <a class="thumbs-down" href="" title="Нет"> Нет</a>
-                            </div>
-                        </div>
                         <!-- end:Main Form -->
+                    </div>
+                    <div class="places-please">
+                        <p>Если в процессе прогулки по нашему маршруту вы сделали хорошие фотографии, мы будем
+                            признательны, если к публикации вы добавите хэштег <b>#WalkIsFun</b>.</p>
                     </div>
                 </div>
                 <div class="col-md-3">
@@ -81,7 +55,6 @@
 
 <!--JS section-->
 <script src="/js/home.js"></script>
-<script src="/js/jquery.easy-autocomplete.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
